8e2f2b8e6136cc0dfbecb8c6234f61fff21707b7
Luqmaan <ldawoodjee@gmail.com>
2015-06-07 19:07:07 -0500

    Add trails

--- download.sh
+++ download.sh
@@ -7,6 +7,7 @@ mkdir -p data
 mkdir -p data/park
 mkdir -p data/amenity
 mkdir -p data/facility
+mkdir -p data/trail
 
 city_of_austin_parks="http://services.arcgis.com/0L95CJ0VTaxqcmED/ArcGIS/rest/services/city_of_austin_parks/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pgeojson&token="
 curl $city_of_austin_parks | jq '' - > raw/city_of_austin_parks.json
@@ -16,6 +17,9 @@ curl $pard_amenity_points | jq '' - > raw/pard_amenity_points.json
 
 pard_facility_points="https://services.arcgis.com/0L95CJ0VTaxqcmED/ArcGIS/rest/services/STRUCTURE_pard_facility_points/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pgeojson&token="
 curl $pard_facility_points | jq '' - > raw/pard_facility_points.json
+
+pard_trails_nrpa="http://services.arcgis.com/0L95CJ0VTaxqcmED/ArcGIS/rest/services/pard_trails_nrpa/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pgeojson&token="
+curl $pard_trails_nrpa | jq '' - > raw/pard_trails_nrpa.json
 
 python generate_parks.py
 
--- generate_parks.py
+++ generate_parks.py
@@ -75,6 +75,31 @@ def generate_facility(cursor):
             fh.write(json.dumps(feature_collection))
 
 
+def generate_trails(cursor):
+    with open('./raw/pard_trails_nrpa.json', 'r') as fh:
+        data = fh.read()
+    data = json.loads(data)
+
+    crs = data['crs']
+    features_by_park = defaultdict(list)
+
+    for feature in data['features']:
+        geometry = less_shitty_geometry(cursor, feature['geometry'], crs)
+        feature['geometry'] = geometry
+        park_id = feature['properties']['PARK_ID']
+
+        features_by_park[park_id].append(feature)
+
+    for park_id, features in features_by_park.items():
+        feature_collection = {
+            'type': 'FeatureCollection',
+            'features': features
+        }
+        with open('data/trail/park_{}.geojson'.format(park_id), 'w+') as fh:
+            fh.write(json.dumps(feature_collection))
+
+
+
 def unshit_parks_topo(cursor):
     with open('./raw/city_of_austin_parks.json', 'r') as fh:
         data = fh.read()
@@ -98,11 +123,12 @@ if __name__ == '__main__':
     conn = psycopg2.connect("dbname='bostongreenmap' user='django' host='localhost' password='django'")
     cursor = conn.cursor()
 
-    generate_park(cursor)
-    generate_amenity(cursor)
-    generate_facility(cursor)
+    # generate_park(cursor)
+    # generate_amenity(cursor)
+    # generate_facility(cursor)
+    generate_trails(cursor)
 
-    unshit_parks_topo(cursor)
+    # unshit_parks_topo(cursor)
 
     cursor.close()
     conn.close()
