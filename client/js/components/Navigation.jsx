f1a71d4e5b9f56694959d921cbc939729d68df99
fremn <fremen.deruvo@youearnedit.com>
2015-06-13 20:24:04 -0500

    actually add the navigation

---
+++ client/js/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class ParkMap extends React.Component {
+  // TODO: props for current location
+  render () {
+
+    return (
+      <div><a className='button' href='/'>Home</a></div>
+    );
+  };
+};
