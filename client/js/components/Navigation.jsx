f4dc60399d2dce5728db96ef09d6777e479506cb
Luqmaan <ldawoodjee@gmail.com>
2015-07-12 11:54:07 -0500

    Rename h1 to siteTitle

--- client/js/components/Navigation.jsx
+++ client/js/components/Navigation.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 export default class Navigation extends React.Component {
     render () {
-        var h1 = <h1 onClick={() => window.location.reload()}>Austin Green Map</h1>;
+        var siteTitle = <h1 onClick={() => window.location.reload()}>Austin Green Map</h1>;
 
         return (
             <div className='navigation'>
                 <a title='Austin Green Map' className='logo' onClick={() => window.location.reload()}>
                     <img alt='Austin Green Map' src='images/deciduous_tree.png' />
                 </a>
-                {this.props.children ? this.props.children : h1}
+                {this.props.children ? this.props.children : siteTitle}
             </div>
         );
     }
