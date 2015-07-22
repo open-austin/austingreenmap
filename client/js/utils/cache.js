78c46a5cb5752711da55bfa72188cf544f550765
Preston Pham <prestonp08@gmail.com>
2015-07-22 14:58:52 -0500

    whitespace

--- client/js/utils/cache.js
+++ client/js/utils/cache.js
@@ -1,12 +1,12 @@
-var _cache = {};
+let _cache = {};
 
 if (localStorage) {
-  _cache = localStorage;
+    _cache = localStorage;
 }
 
-var cache = {
-  get: (key) => _cache[key] ? JSON.parse(_cache[key]) : null,
-  set: (key, val) => _cache[key] = JSON.stringify(val)
+const cache = {
+    get: (key) => _cache[key] ? JSON.parse(_cache[key]) : null,
+    set: (key, val) => _cache[key] = JSON.stringify(val)
 };
 
 export default cache;
