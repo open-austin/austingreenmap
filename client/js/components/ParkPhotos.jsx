82b5936943464c227489ad9ef052bbb1170153d6
Preston Pham <prestonp08@gmail.com>
2015-07-29 18:05:40 -0500

    add styling

--- client/js/components/ParkPhotos.jsx
+++ client/js/components/ParkPhotos.jsx
@@ -9,7 +9,8 @@ export default class ParkPage extends React.Component {
 
         this.state = {
             loading: true,
-            photos: []
+            photos: [],
+            idx: 0
         };
     }
 
@@ -28,15 +29,13 @@ export default class ParkPage extends React.Component {
     }
 
     next() {
-        let photos = this.state.photos;
-        photos.unshift(photos.pop());
-        this.setState({photos: photos});
+        const idx = ++this.state.idx % this.state.photos.length;
+        this.setState({idx: idx});
     }
 
     prev() {
-        let photos = this.state.photos;
-        photos.push(photos.shift());
-        this.setState({photos: photos});
+        let idx = this.state.idx - 1 < 0 ? this.state.photos.length-1 : --this.state.idx;
+        this.setState({idx: idx});
     }
 
     render() {
@@ -44,10 +43,11 @@ export default class ParkPage extends React.Component {
 
         return (
             <div>
-              <img src={this.state.photos[0]} />
+              <img className="park-photo" src={this.state.photos[this.state.idx]} />
               <div>
-                <span onClick={() => this.prev()}>&lt;</span>
-                <span onClick={() => this.next()}>&gt;</span>
+                <span className="park-photo-pager" onClick={(e) => this.prev(e)}>&lt;</span>
+                <span> {this.state.idx + 1} / {this.state.photos.length} </span>
+                <span className="park-photo-pager" onClick={(e) => this.next(e)}>&gt;</span>
               </div>
             </div>
         );
