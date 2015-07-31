89b1af064c555af9304d5fe512bf3a784803315d
Preston Pham <prestonp08@gmail.com>
2015-07-30 20:00:21 -0500

    add react-slick carousel

--- client/js/components/ParkPhotos.jsx
+++ client/js/components/ParkPhotos.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import when from 'when';
 import api from '../utils/api';
+import Slider from 'react-slick';
 
 export default class ParkPhotos extends React.Component {
     constructor(props) {
@@ -14,8 +15,9 @@ export default class ParkPhotos extends React.Component {
     }
 
     load() {
+      // TODO handle thumbnail size upstream / interop with zoom.js
       const photosPromise = api.getParkPhotos(this.props.parkId)
-          .tap((data) => this.setState({photos: data}));
+          .tap((data) => this.setState({photos: data.map((url) => url.replace('.jpg', '_q.jpg'))}));
 
       when.settle([photosPromise])
           .then(() => {
@@ -40,15 +42,28 @@ export default class ParkPhotos extends React.Component {
     render() {
         if (!this.state.photos.length) { return (<div></div>); }
 
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            centerMode: true,
+            arrows: true,
+            responsive: [
+                { breakpoint: 768, settings: { slidesToShow: 2 } },
+                { breakpoint: 961, settings: { slidesToShow: 3 } },
+                { breakpoint: 1024, settings: { slidesToShow: 4 } },
+                { breakpoint: 100000, settings: { slidesToShow: 5 } }
+            ]
+        };
+
+        const contents = this.state.photos.map( (photo) =>
+            ( <div><img src={photo} /></div> )
+        );
+
         return (
-            <div>
-                <img className="park-photo" src={this.state.photos[this.state.idx]} />
-                <div>
-                    <span className="park-photo-pager" onClick={(e) => this.prev(e)}>&lt;</span>
-                    <span> {this.state.idx + 1} / {this.state.photos.length} </span>
-                    <span className="park-photo-pager" onClick={(e) => this.next(e)}>&gt;</span>
-                </div>
-            </div>
+            <Slider className="park-photos" {...settings}>
+                {contents}
+            </Slider>
         );
     }
 }
